90c885a4c4c388b3f0bc182a968a71c33de8aca4
scottmotte <scott@scottmotte.com>
2014-06-28 07:06:22 -0600

    working

--- app.js
+++ app.js
@@ -1,29 +1,61 @@
 var dotenv = require('dotenv');
 dotenv.load();
 
-var Hapi   = require('hapi');
-var port   = parseInt(process.env.PORT) || 3000;
+var SENDGRID_USERNAME = process.env.SENDGRID_USERNAME;
+var SENDGRID_PASSWORD = process.env.SENDGRID_PASSWORD;
+var SECRET_NUMBER = process.env.SECRET_NUMBER;
+var PRIZE = "<img src='http://www.maxistentialism.com/bees/oprahbees.gif'/>";
+var Hapi = require('hapi');
+var sendgrid = require('sendgrid')(SENDGRID_USERNAME, SENDGRID_PASSWORD);
+var port = parseInt(process.env.PORT) || 3000;
+var redis = require("redis");
+var client = redis.createClient();
+client.on("error", function (err) {
+  console.log("Error " + err);
+});
+
+function saveGuess(from, subject, callback) {
+  var parts = from.split(/[<>]/);
+  var name = parts[0], email = parts[1];
+  client.set(email, subject);
+  callback(email, subject);
+};
 
 // Create a server with a host and port
 var server = Hapi.createServer('localhost', port);
-
-// Add the html routes
-server.route({
-  method: 'GET',
-  path: '/{path*}',
-  handler: {
-    directory: { path: './public', listing: false, index: true }
-  }
-});
 
 // Add the inbound route
 server.route({
   method: 'POST',
   path:   '/inbound',
   handler: function (request, reply) {
-    console.log(request.payload);
+    // Email guess to: hi@mot.webhook.email
+    var payload = request.payload;
+    var subject = payload.subject;
+    var email = payload.from;
 
-    reply({success: true});
+    saveGuess(email, subject, function(email, subject) {
+      console.log(subject);
+
+      // step 2
+      var html = "Wrong number. Try again";
+      if (subject == SECRET_NUMBER) {
+        html = "CORRECT! You win a prize.<br/>";
+        html += PRIZE;
+      }
+
+      sendgrid.send({
+        to: email,
+        from: 'hi@mot.webhook.email',
+        subject: 'Thanks for coming to PayPal BattleHack Chicago!',
+        html: html
+      }, function(err, resp) {
+        console.log(err);
+        console.log(resp);
+      });
+
+      reply({success: true});
+    });
   }
 });
 
